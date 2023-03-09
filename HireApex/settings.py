f52072834c08bc22d558c0b0dc8f20f571d12fed
annansadr22 <126930908+annansadr22@users.noreply.github.com>
2023-03-09 00:26:54 -0500

    update

--- HireApex/settings.py
+++ HireApex/settings.py
@@ -142,10 +142,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [BASE_DIR / "static",]
+#STATICFILES_DIRS = [BASE_DIR / "static",]
 import os
-#STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 # Default primary key field type
